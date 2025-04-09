57c26dc8d55d51932f2e26e8a64445e746ad8785
Gordus Alexander <aldrmiller@gmail.com>
2025-04-09 20:50:57 +0600

    AssignChatBot-RC v1.0

---
+++ index.js
@@ -0,0 +1,49 @@
+// AssignChatBot-RC v1.0
+// By @aldrrider - Telegram
+// Данный скрипт разработан для автоматизации сбора заявок из общей очереди чата, программы RocketChat.
+
+let isProcessing = false;
+
+const GetElements = () => {
+    const containerRoom = document.querySelector('.rooms-list__list.inquiries');
+    const elementsRoom =  containerRoom.querySelectorAll('.sidebar-item');
+    const elementsLink =  elementsRoom.querySelectorAll('.sidebar-item__link');
+    const buttonRoom =  document.querySelector('.rc-message-box__take-it-button');
+
+    return { containerRoom, elementsRoom, elementsLink, buttonRoom }
+};
+
+const handlerClick = () => {
+    if (isProcessing) return;
+    isProcessing = true;
+
+    const { containerRoom, elementsRoom, elementsLink, buttonRoom} = GetElements();
+
+    if (!containerRoom || !elementsRoom || elementsLink.length === 0) {
+        isProcessing = false;
+        return;
+    }
+
+    const handlerClickNewChat = (i) => {
+        setInterval(() => {
+            elementsLink[i].click();
+            if (buttonRoom) {
+                buttonRoom.click();
+            }
+        }, 300);
+     }
+
+    for (let i = 0; i < elementsLink.length; i++) {
+        handlerClickNewChat(i);
+    }
+
+    isProcessing = false;
+};
+
+const startScript = () => {
+    setInterval(() => {
+      handlerClick();
+    }, 1000);
+}
+
+startScript();
